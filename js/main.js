18f8ac5e57dce35e164699366f94f5386ce7a345
Atari361 <letsky1994@gmail.com>
2017-05-17 22:02:12 +0300

    smooth scroll added

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    // Add scrollspy to <body>
+    $('body').scrollspy({target: ".navbar-fixed-top", offset: 50});
+
+    // Add smooth scrolling on all links inside the navbar
+    $("#bs-example-navbar-collapse-1 a").on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top + -50
+        }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+
+      } // End if
+
+    });
+});
